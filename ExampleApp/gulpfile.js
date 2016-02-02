2c58330503902763d34080650fe5453edee65351
bpineda <_@bernardopineda.mx>
2016-02-02 15:32:28 -0600

    Added the file to be loaded (concatenated) to the app.js file

--- ExampleApp/gulpfile.js
+++ ExampleApp/gulpfile.js
@@ -45,7 +45,8 @@ var paths = {
   // These files are for your app's JavaScript
   appJS: [
     'client/assets/js/app.js',
-    'client/assets/js/controllers/home.js'
+    'client/assets/js/controllers/home.js',
+    'client/assets/js/controllers/about.js'
   ]
 }
 
