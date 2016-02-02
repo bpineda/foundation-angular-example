40e9d2e191846b3fa8369cfba1c569118255777d
bpineda <_@bernardopineda.mx>
2016-02-02 15:08:19 -0600

    Added controllers directory and home.js file with the HomeController

---
+++ ExampleApp/client/assets/js/controllers/home.js
@@ -0,0 +1,31 @@
+(function() {
+  'use strict';
+
+  angular.module('application.home', [
+    
+  ])
+  .controller('HomeController', [ '$scope', 
+                                  '$stateParams', 
+                                  '$state', 
+                                  '$controller',
+                                  function(
+                                            $scope,
+                                            $stateParams,
+                                            $state,
+                                            $controller)
+                                  {
+
+                                    $scope.entity = {};
+
+                                    $scope.show_message = function()
+                                    {
+                                      
+                                    };
+      
+
+    }])
+    
+    
+  ;
+
+})();
